b416491bbbb6fe956629425d80a1ac17730071cc
Lusyluh <mav19003@byui.edu>
2022-04-24 00:38:32 +0200

    webserver updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Lusanda Magadla');
+  res.send('Mpho Frank');
 });
 
 app.listen(port, () => {
