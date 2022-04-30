720af5effd19ae43f166c45c393c8a87d1504abb
Lusyluh <mav19003@byui.edu>
2022-04-30 23:18:01 +0200

    testing connection

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use('/', require('./routes'));
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
   console.log('what is going on');
+  console.log('this is frustrating');
 });
 
 //connection to the mongo database
